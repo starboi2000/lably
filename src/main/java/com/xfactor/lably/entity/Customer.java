678303636d2cbf619653e6bef9b11b3fe9bf9337
starboi2000 <anikchatterjee63@gmail.com>
2021-05-16 12:37:23 +0530

    hurray

---
+++ src/main/java/com/xfactor/lably/entity/Customer.java
@@ -0,0 +1,45 @@
+package com.xfactor.lably.entity;
+
+public class Customer {
+
+    private String name;
+
+    private String age;
+
+    private String address;
+
+    private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getage() {
+        return age;
+    }
+
+    public void setage(String age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getgender() {
+        return gender;
+    }
+
+    public void setgender(String gender) {
+        this.gender = gender;
+    }
+
+}
---
+++ src/main/java/com/xfactor/lably/entity/Tests.java
@@ -0,0 +1,40 @@
+package com.xfactor.lably.entity;
+
+public class Tests {
+
+    private String name;
+
+
+    private String Description;
+
+    private Double price;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+}
+
+    
